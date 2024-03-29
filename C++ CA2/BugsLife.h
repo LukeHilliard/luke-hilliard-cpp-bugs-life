d9436e18fafc411d9f4f278599c5bfea6e2eda78
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-03-29 12:34:18 +0000

    Basic setup
    Basic menu system created, does not display different options yet.
    
    Header file created and included

---
+++ C++ CA2/BugsLife.h
@@ -0,0 +1,13 @@
+//
+// Created by Luke Hilliard on 29/03/2024.
+//
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+typedef void (*func)();
+
+#ifndef C_CA2_BUGSLIFE_H
+#define C_CA2_BUGSLIFE_H
+
+#endif //C_CA2_BUGSLIFE_H
--- C++ CA2/main.cpp
+++ C++ CA2/main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+
+#include "BugsLife.h" // header file
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    map<int, func> menuOptions;
+
+    // TODO add calls to methods for each menu option
+//    menuOptions[1] = ;
+//    menuOptions[2] = ;
+//    menuOptions[3] = ;
+//    menuOptions[4] = ;
+//    menuOptions[5] = ;
+//    menuOptions[6] = ;
+//    menuOptions[7] = ;
+//    menuOptions[8] = ;
+
+    int choice = 0;
+    string temp;
+    do {
+        cout << "Please enter an option from the menu (-1 to end): ";
+        cin >> choice;
+        getline(cin, temp);
+        cout << endl;
+        if(menuOptions.count(choice))
+        {
+            menuOptions[choice]();
+        }
+        else if(choice!=-1)
+        {
+            cout << "Invalid input, please select a corresponding number from the menu"<<endl;
+        }
+        cout << "\n\n\n";
+    }while(choice!=-1);
     return 0;
 }
